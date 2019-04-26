3eff0a508f043eff208fc4a74d345408cd5c2bbc
og-bash-scripts <48135304+og-bash-scripts@users.noreply.github.com>
2019-04-26 09:19:39 -1000

    Update 1-2a.expect_ssh.sh

--- 1-2a.expect_ssh.sh
+++ 1-2a.expect_ssh.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/expect
 
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+set ssh_host "ubuntu1" #[lindex $argv 2]
+
+spawn ssh $username@$ssh_host
+
+expect "$username@$ssh_host\'s password:" {send "$password\r"}
+
+expect "$ "
+send "ls -l /etc\n"
+send "exit\n"
+
+#Without interact, the above send commands are not run
+interact
