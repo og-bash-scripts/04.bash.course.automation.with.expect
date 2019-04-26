91ed12ba2dc5fc11b6e075b3f03fa69070044b69
og-bash-scripts <48135304+og-bash-scripts@users.noreply.github.com>
2019-04-26 09:21:29 -1000

    Update 2-1a.rsync.sh

--- 2-1a.rsync.sh
+++ 2-1a.rsync.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/expect
 
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+set ssh_host "ubuntu1"
+
+spawn rsync -avz $username@$ssh_host:/datafiles/salesdata /backup
+
+expect "$username@$ssh_host's password:" {send $password\r}
+
+#The command failes without the following interact statement
+interact
