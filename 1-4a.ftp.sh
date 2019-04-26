62356c0d193d84c9ba978443a3e52479e7b26cc0
og-bash-scripts <48135304+og-bash-scripts@users.noreply.github.com>
2019-04-26 09:20:27 -1000

    Update 1-4a.ftp.sh

--- 1-4a.ftp.sh
+++ 1-4a.ftp.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+clear
+server="10.0.1.4"
+echo
+read -p "Enter FTP username: " ftpusername
+read -p "Enter password: " ftppassword #password sent in plain text, displayed on stdout, mask it
 
+ftp -i -n -v $server << EOT
+user $ftpusername $ftppassword
+ascii
+cd /srv/ftp
+lcd /incoming
+mget *.tar
+bye
+EOT
