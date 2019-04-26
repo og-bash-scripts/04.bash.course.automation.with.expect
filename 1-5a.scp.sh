a6a36c0fa562e3a64ef76daf82fa3871abb4d8db
og-bash-scripts <48135304+og-bash-scripts@users.noreply.github.com>
2019-04-26 09:20:57 -1000

    Update 1-5a.scp.sh

--- 1-5a.scp.sh
+++ 1-5a.scp.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/expect
 
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+set ssh_host "ubuntu1"
+
+spawn scp $username@ssh_host:/datafiles/monthly_numbers.txt /backup/remotehost
+
+expect "$username@ssh_host's pasword:" {send "password\r"}
+
+#The command fails without the following interact statement
+interact
